f360f559248be0ce6b9c80efaa28e1dfef61849c
Ali Deeb <ali1alideeb@gmail.com>
2024-09-19 16:54:13 +0300

    added floation add button.

--- lib/features/home/view/home/home_content_view.dart
+++ lib/features/home/view/home/home_content_view.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import '../../../../core/common/external_storage_handler.dart';
 import '../../../../core/navigation/nav.dart';
 import '../../../../core/theme/text_theme_styles.dart';
+import '../../../../core/ui/widgets/custom_scaffold.dart';
 import '../../view_model/home_view_model.dart';
 import '../folder_file/folder_file_view.dart';
 
@@ -26,40 +27,50 @@ class _HomeContentViewState extends State<HomeContentView> {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
-      itemCount: vm.rootFoldersAndFiles.length,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
+    return CustomScaffold(
+      body: GridView.builder(
+        padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+        itemCount: vm.rootFoldersAndFiles.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        itemBuilder: (context, index) {
+          return InkWell(
+            onTap: () {
+              Nav.to(
+                FolderFileView.routeName,
+                arguments: FolderFileViewParam(
+                  path: vm.rootFoldersAndFiles[index].path,
+                ),
+              );
+            },
+            child: Container(
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.amber,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Text(
+                vm.rootFoldersAndFiles[index].path.split("/").last,
+                style: TextThemeStyles.text_14_Regular.copyWith(
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          );
+        },
       ),
-      itemBuilder: (context, index) {
-        return InkWell(
-          onTap: () {
-            Nav.to(
-              FolderFileView.routeName,
-              arguments: FolderFileViewParam(
-                path: vm.rootFoldersAndFiles[index].path,
-              ),
-            );
-          },
-          child: Container(
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: Colors.amber,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Text(
-              vm.rootFoldersAndFiles[index].path.split("/").last,
-              style: TextThemeStyles.text_14_Regular.copyWith(
-                color: Colors.white,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-        );
-      },
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).primaryColor,
+        onPressed: () {},
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
     );
   }
 }
