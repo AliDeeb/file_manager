328eeaf9717ee53550158ac3c56119aee47bf1be
Ali Deeb <ali1alideeb@gmail.com>
2024-09-22 16:10:13 +0300

    added empty widget.

--- lib/features/home/view/folder_file/folder_file_content_view.dart
+++ lib/features/home/view/folder_file/folder_file_content_view.dart
@@ -38,80 +38,95 @@ class _FolderFileContentViewState extends State<FolderFileContentView> {
             ),
           ),
         ),
-        Expanded(
-          child: GridView.builder(
-            padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
-            itemCount: vm.foldersAndFiles.length,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
+        if (vm.foldersAndFiles.isEmpty)
+          Expanded(
+            child: Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                "There are no folders or files to display.",
+                style: TextThemeStyles.text_26_Bold.copyWith(
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.center,
+              ),
             ),
-            itemBuilder: (context, index) {
-              final name = vm.foldersAndFiles[index].path.split("/").last;
-              final isFile = vm.foldersAndFiles[index].statSync().type ==
-                  FileSystemEntityType.file;
+          ),
+        if (vm.foldersAndFiles.isNotEmpty)
+          Expanded(
+            child: GridView.builder(
+              padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+              itemCount: vm.foldersAndFiles.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+              ),
+              itemBuilder: (context, index) {
+                final name = vm.foldersAndFiles[index].path.split("/").last;
+                final isFile = vm.foldersAndFiles[index].statSync().type ==
+                    FileSystemEntityType.file;
 
-              return GestureDetector(
-                onLongPress: () {
-                  showMenu<int>(
-                    context: context,
-                    position: const RelativeRect.fromLTRB(0, 0, 0, 0),
-                    items: [
-                      PopupMenuItem(
-                        child: const Text("Details"),
-                        onTap: () {
-                          showFolderFileDetailsDialog(
-                            vm.foldersAndFiles[index].statSync(),
-                            name,
-                          );
-                        },
+                return GestureDetector(
+                  onLongPress: () {
+                    showMenu<int>(
+                      context: context,
+                      position: const RelativeRect.fromLTRB(0, 0, 0, 0),
+                      items: [
+                        PopupMenuItem(
+                          child: const Text("Details"),
+                          onTap: () {
+                            showFolderFileDetailsDialog(
+                              vm.foldersAndFiles[index].statSync(),
+                              name,
+                            );
+                          },
+                        ),
+                        PopupMenuItem(
+                          child: const Text("Delete"),
+                          onTap: () {
+                            vm.onDeleteTap(index);
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                  onTap: () => vm.onFolderFileTap(index),
+                  child: Column(
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: Container(
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: isFile ? Colors.blueGrey : Colors.amber,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Icon(
+                            isFile ? Icons.file_copy : Icons.folder_outlined,
+                            color: Colors.white,
+                          ),
+                        ),
                       ),
-                      PopupMenuItem(
-                        child: const Text("Delete"),
-                        onTap: () {
-                          vm.onDeleteTap(index);
-                        },
+                      Expanded(
+                        flex: 1,
+                        child: Align(
+                          child: Text(
+                            name,
+                            style: TextThemeStyles.text_14_Regular.copyWith(
+                              color: Colors.black,
+                            ),
+                            textAlign: TextAlign.center,
+                            maxLines: 2,
+                          ),
+                        ),
                       ),
                     ],
-                  );
-                },
-                onTap: () => vm.onFolderFileTap(index),
-                child: Column(
-                  children: [
-                    Expanded(
-                      flex: 2,
-                      child: Container(
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          color: isFile ? Colors.blueGrey : Colors.amber,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Icon(
-                          isFile ? Icons.file_copy : Icons.folder_outlined,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Align(
-                        child: Text(
-                          name,
-                          style: TextThemeStyles.text_14_Regular.copyWith(
-                            color: Colors.black,
-                          ),
-                          textAlign: TextAlign.center,
-                          maxLines: 2,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            },
+                  ),
+                );
+              },
+            ),
           ),
-        ),
       ],
     );
   }
